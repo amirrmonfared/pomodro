e4379b2123e6adb5015f7f44917f0b29f9a9622b
amirrmonfared <amir.monfared@adjust.com>
2024-10-10 19:21:14 +0200

    Initial Commit

---
+++ PomodroTests/PomodroTests.swift
@@ -0,0 +1,16 @@
+//
+//  PomodroTests.swift
+//  PomodroTests
+//
+//  Created by Amir Monfared on 10.10.24.
+//
+
+import Testing
+
+struct PomodroTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
